669402859d0d06320e16fbe3a2a5f83d0bd478b0
majidgeeks <majidrajput246@gmail.com>
2024-03-24 22:33:42 +0500

    initilized project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {  View, SafeAreaView } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+
+
+
+const App = () => {
+  return (
+      <LoginScreen/>
+  );
+};
+
+export default App;
+
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface LoginScreenProps {}
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1
+  }
+});
