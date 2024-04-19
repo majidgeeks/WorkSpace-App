e667dfa5a7e3b09aa4288781b5bb136e15cc37e9
majidgeeks <majidrajput246@gmail.com>
2024-04-20 02:55:41 +0500

    profile screen completed with functionality and some neccessary changes in other screens

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-import Navigater from './src/Navigator/Navigater';
 import SelectFlowScreen from './src/screens/SelectFlowScreen';
 import SelectComScreen from './src/screens/SelectComScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './src/Navigator/AuthNavigater';
+
 
 
 
@@ -12,10 +14,13 @@ const App = () => {
   return (
       // <LoginScreen/>
       // <SignUpScreen />
-      // <Navigater />
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer> 
       // <SelectFlowScreen />
       // <SelectComScreen />
-      <ProfileScreen />
+      // <ProfileScreen />
+      
   );
 };
 
